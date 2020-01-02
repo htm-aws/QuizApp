57be24fcc1ccb182682d4da9735c9f5e96e76f98
hoangtrongminh <htm.dev304@gmail.com>
2020-01-02 16:36:35 +0700

    add score++

--- js/game.js
+++ js/game.js
@@ -3,6 +3,9 @@ const question = document.getElementById('question')
 const choices = Array.from(document.getElementsByClassName("choice-text"));
 console.log(question)
 
+const questionCounter = document.getElementById('questionCounter');
+const scoreText = document.getElementById('score');
+
 let currQuestion = {};
 let acceptingAnswers = false;
 let score = 0;
@@ -25,12 +28,28 @@ let  questions = [
         choice3: "Amazon S3",
         choice4: "AWS Lambda",
         answer: 1
+    },
+    {
+        question: "A legacy application needs to interact with local storage using iSCSI. A team needs to design a reliable storage solution to provision all new storage on AWS.\nWhich storage solution meets the legacy application requirements?",
+        choice1: "AWS Snowball storage for the legacy application until the application can be re-architected",
+        choice2: "AWS Storage Gateway in cached mode for the legacy application storage to write data to Amazon S3",
+        choice3: "AWS Storage Gateway in stored mode for the legacy application storage to write data to Amazon S3",
+        choice4: "An Amazon S3 volume mounted on the legacy application server locally using the File Gateway service",
+        answer: 3
+    },
+    {
+        question: "A Solutions Architect is designing an architecture for a mobile gaming application. The application is expected to be very popular. The Architect needs to prevent the Amazon RDS MySQL database from becoming a bottleneck due to frequently accessed queries.\nWhich service or feature should the Architect add to prevent a bottleneck?",
+        choice1: "Multi-AZ feature on the RDS MySQL Database",
+        choice2: "ELB Classic Load Balancer in front of the web application tier",
+        choice3: "Amazon SQS in front of RDS MySQL Database",
+        choice4: "Amazon ElastiCache in front of the RDS MySQL Database",
+        answer: 4
     }
 ]
 
 // Constants
-const CORRECT_BONUS = 10;
-const MAX_QUESTION = 2;
+const MAX_QUESTION = 4;
+const CORRECT_BONUS = 100/MAX_QUESTION;
 
 startGame = () => {
     questionCount = 0;
@@ -46,6 +65,8 @@ getNewQuestion = () => {
     }
 
     questionCount++;
+    questionCounter.innerHTML = `${questionCount}/${MAX_QUESTION}`;
+
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currQuestion = availableQuestions[questionIndex];
     question.innerText = currQuestion.question;
@@ -70,7 +91,11 @@ choices.forEach(choice => {
         const selectedAnswer = selectedChoice.dataset['number'];
 
         const classToApply = (selectedAnswer == currQuestion.answer) ? 'correct' : 'incorrect';
- 
+        
+        if (classToApply === "correct") {
+            incrementScore(CORRECT_BONUS);
+        }
+        
         console.log(classToApply);
 
         selectedChoice.parentElement.classList.add(classToApply);
@@ -82,4 +107,8 @@ choices.forEach(choice => {
     })
 })
 
+incrementScore = num => {
+    score += num;
+    scoreText.innerHTML = score;
+}
 startGame();
